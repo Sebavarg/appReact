5c04bb9fda9312d482a3df0ceb53b02c59a7e1a2
Sebastian Vargas <vargaszsebastian@gmail.com>
2022-11-11 21:23:48 -0300

    styles primer entrega Vargas Zingarretti

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,8 +1,8 @@
 import carrito from "../assets/carrito.svg";
+import style from "../CartWidget/cartWidget.css"
 
 export const CartWidget = () =>{
     return (
         <img src={carrito}></img>
-    );
-    
+    )
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,25 +1,21 @@
-import React from "react";
-
+import React from 'react'
+import style from "../NavBar/navBar.css";
 import {CartWidget} from "../CartWidget/CartWidget";
-
-
+import logo from "../assets/logo.svg";
 
 export const NavBar = () => {
   return (
-    <nav className="navbar navbar-expand-lg fixed-lg-top">
-     <div className="container-fluid">
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse justify-content-evenly" id="navbarSupportedContent">
-            <div className="navbar-nav">
-                <a className="nav-link px-5 fst-italic" href="../index.html">PRODUCTOS</a>
-                <a className="nav-link px-5 fst-italic" href="company.html">COMPRAR</a>
-                <a className="nav-link px-5 fst-italic" href="news.html">MEDIOS DE PAGO</a>
-                <a className="nav-link px-5 fst-italic" href="contact.html"><CartWidget /></a>  
-            </div>
-        </div>
-     </div>
-    </nav>
-  );
-};
+    <nav>
+      <a className="navbar-brand" href="#">
+        <img src={logo}></img>
+      </a>
+      <ul>
+        <li><a href="#">Productos</a></li>
+        <li><a href="#">Favoritos</a></li>
+        <li><a href="#">Promociones y Cupones </a></li>
+        <li><a href="#">Contacto</a></li>
+      </ul>
+      <a href="#"><CartWidget /></a>
+   </nav>
+  )
+}
